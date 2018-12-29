4e998b0e8fc161db85f3b09c4c53e821cafe9b3c
snadler <sharonn@dropbox.com>
2018-12-30 01:29:07 +0200

    Added a basic board.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+  constructor() {
+	  super()
+	  this.state = {
+			board_size : 3,
+    }
+    
+    
+	}
+
+  onClickHandler = (e) => {
+  }
+
+  render_row  = (row_num) =>  {
+    let row_burrons = []
+    for (let j = 0; j < this.state.board_size; j++) {
+      row_burrons.push(<button className='square-button' onClick={this.onClickHandler}> {row_num} </button>)
+    }
+    return (
+      <div className='row'>
+        {row_burrons}
+      </div>
+    )
+  }
+
+  render_board = (row_num) =>  {
+    let rows = []
+    for (let j = 0; j < this.state.board_size; j++) {
+      rows.push(this.render_row(j+1))
+    }
+    return (
+      <div className='board'>
+        {rows}
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        {this.render_board()}
       </div>
     );
   }
 }
+
+
+
+
+
+
 
 export default App;
