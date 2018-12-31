811860a20209d4ebb5a50cabce82b674a219ecf8
snadler <sharonn@dropbox.com>
2018-12-31 18:33:36 +0200

    Updates

---
+++ precache-manifest.2da6241478146b3fcac154952aa9c769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/[your-repo-name]/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ea93e6a2909e2dcb173",
+    "url": "/[your-repo-name]/static/js/runtime~main.6ea93e6a.js"
+  },
+  {
+    "revision": "3874508fd2f4adf40438",
+    "url": "/[your-repo-name]/static/js/main.3874508f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/[your-repo-name]/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3874508fd2f4adf40438",
+    "url": "/[your-repo-name]/static/css/main.c0bb4860.chunk.css"
+  },
+  {
+    "revision": "b5eaa353ba8e5e0888fd39e470ec37ba",
+    "url": "/[your-repo-name]/index.html"
+  }
+];
